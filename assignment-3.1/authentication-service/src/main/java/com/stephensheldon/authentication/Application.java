03b4adaf8fead6e8fa29a1abcd23723a17027654
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-23 14:57:01 -0400

    Created main class for auth server

--- assignment-3.1/authentication-service/src/main/java/com/stephensheldon/authentication/Application.java
+++ assignment-3.1/authentication-service/src/main/java/com/stephensheldon/authentication/Application.java
@@ -1,7 +1,39 @@
 package com.stephensheldon.authentication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Author: Stephen Sheldon
  **/
+
+@SpringBootApplication
+@RestController
+@EnableAuthorizationServer
+@EnableResourceServer
 public class Application {
+
+    @RequestMapping(value = {"/user"}, produces = "application/json")
+    public Map<String, Object> user(OAuth2Authentication user) {
+        Map<String, Object> userInfo = new HashMap<>();
+
+        // Retrieve the passed OAuth2Authentication user data and store in hashmap
+        userInfo.put("user", user.getUserAuthentication().getPrincipal());
+        userInfo.put("authorities", AuthorityUtils.authorityListToSet(user.getUserAuthentication().getAuthorities()));
+
+        return userInfo;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
