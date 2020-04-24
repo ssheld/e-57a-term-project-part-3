71dda63e19b44d870d154e6ca6bbe42ea2877713
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-24 08:55:55 -0400

    Set up Hystrix for auth service

---
+++ assignment-3.1/authentication-service/src/main/java/com/stephensheldon/authentication/hystrix/DelegatingUserContextCallable.java
@@ -0,0 +1,49 @@
+package com.stephensheldon.authentication.hystrix;
+
+import com.stephensheldon.authentication.utils.UserContext;
+import com.stephensheldon.authentication.utils.UserContextHolder;
+import org.springframework.util.Assert;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Author: Stephen Sheldon
+ **/
+public final class DelegatingUserContextCallable<V> implements Callable<V> {
+
+    private final Callable<V> delegate;
+
+    private UserContext originalUserContext;
+
+    public DelegatingUserContextCallable(Callable<V> delegate,
+                                         UserContext userContext) {
+        Assert.notNull(delegate, "delegate cannot be null");
+        Assert.notNull(userContext, "userContext cannot be null");
+        this.delegate = delegate;
+        this.originalUserContext = userContext;
+    }
+
+    public DelegatingUserContextCallable(Callable<V> delegate) {
+        this(delegate, UserContextHolder.getContext());
+    }
+
+    public V call() throws Exception {
+        UserContextHolder.setContext(originalUserContext);
+
+        try {
+            return delegate.call();
+        } finally {
+            this.originalUserContext = null;
+        }
+    }
+
+    public String toString() {
+        return delegate.toString();
+    }
+
+
+    public static <V> Callable<V> create(Callable<V> delegate,
+                                         UserContext userContext) {
+        return new DelegatingUserContextCallable<>(delegate, userContext);
+    }
+}
