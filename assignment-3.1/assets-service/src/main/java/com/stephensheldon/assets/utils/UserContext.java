a1167f8dfe6b92b32ef39819b5706b2140ce6f1a
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-24 08:28:16 -0400

    Expanded method for syntactic sugar

--- assignment-3.1/assets-service/src/main/java/com/stephensheldon/assets/utils/UserContext.java
+++ assignment-3.1/assets-service/src/main/java/com/stephensheldon/assets/utils/UserContext.java
@@ -9,16 +9,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserContext {
     public static final String CORRELATION_ID = "tmx-correlation-id";
-    public static final String AUTH_TOKEN     = "tmx-auth-token";
-    public static final String USER_ID        = "tmx-user-id";
-    public static final String ORG_ID         = "tmx-org-id";
+    public static final String AUTH_TOKEN = "tmx-auth-token";
+    public static final String USER_ID = "tmx-user-id";
+    public static final String ORG_ID = "tmx-org-id";
 
-    private String correlationId= new String();
-    private String authToken= new String();
+    private String correlationId = new String();
+    private String authToken = new String();
     private String userId = new String();
     private String orgId = new String();
 
-    public String getCorrelationId() { return correlationId;}
+    public String getCorrelationId() {
+        return correlationId;
+    }
 
     public void setCorrelationId(String correlationId) {
         this.correlationId = correlationId;
