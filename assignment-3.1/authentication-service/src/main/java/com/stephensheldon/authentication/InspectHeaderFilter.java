4a85649bd6919e25ffc4e9eb875835ab06926850
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-24 10:07:22 -0400

    Created InspectHeaderFilter class

---
+++ assignment-3.1/authentication-service/src/main/java/com/stephensheldon/authentication/InspectHeaderFilter.java
@@ -0,0 +1,37 @@
+package com.stephensheldon.authentication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Author: Stephen Sheldon
+ **/
+
+@Component
+public class InspectHeaderFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(InspectHeaderFilter.class);
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        logger.debug("Hitting the authorization server: " + httpServletRequest.getHeader("Authorization"));
+
+        filterChain.doFilter(httpServletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
