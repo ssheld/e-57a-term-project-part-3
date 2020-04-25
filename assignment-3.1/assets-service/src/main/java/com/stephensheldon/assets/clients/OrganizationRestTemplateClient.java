e68fb720634e55b1983a0094a2b5c38ec87e1b29
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-25 08:33:58 -0400

    Updated to use OAuth2RestTemplate

--- assignment-3.1/assets-service/src/main/java/com/stephensheldon/assets/clients/OrganizationRestTemplateClient.java
+++ assignment-3.1/assets-service/src/main/java/com/stephensheldon/assets/clients/OrganizationRestTemplateClient.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Author: Stephen Sheldon
