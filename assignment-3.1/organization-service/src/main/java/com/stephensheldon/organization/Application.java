d1c29adfd8e713626ca1a4c3088c5ba7793bb074
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-25 08:41:18 -0400

    Annotated with @EnableResourceServer

--- assignment-3.1/organization-service/src/main/java/com/stephensheldon/organization/Application.java
+++ assignment-3.1/organization-service/src/main/java/com/stephensheldon/organization/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import javax.servlet.Filter;
 
@@ -16,12 +17,12 @@ import javax.servlet.Filter;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableResourceServer
 public class Application {
 
     @Bean
     public Filter userContextFilter() {
-        UserContextFilter userContextFilter = new UserContextFilter();
-        return userContextFilter;
+        return new UserContextFilter();
     }
 
     public static void main(String[] args) {
