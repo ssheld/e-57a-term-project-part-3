1441eeb6ed9a7abf5e130ff6c34dded8dca5fa07
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-25 08:30:52 -0400

    Created config for authorized users and roles

---
+++ assignment-3.1/assets-service/src/main/java/com/stephensheldon/assets/security/ResourceServerConfiguration.java
@@ -0,0 +1,23 @@
+package com.stephensheldon.assets.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * Author: Stephen Sheldon
+ **/
+
+@Configuration
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/v1/organizations/**")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated();
+    }
+}
