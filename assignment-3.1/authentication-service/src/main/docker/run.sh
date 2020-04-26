15c557566277799976d585d626372b986980568b
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-26 13:41:55 -0400

    Fixed env variable name

--- assignment-3.1/authentication-service/src/main/docker/run.sh
+++ assignment-3.1/authentication-service/src/main/docker/run.sh
@@ -5,11 +5,10 @@ echo "********************************************************"
 while ! `nc -z eurekaserver $EUREKASERVER_PORT`; do sleep 3; done
 echo "******* Eureka Server has started"
 
-
 echo "********************************************************"
 echo "Waiting for the database server to start on port $DATABASE_PORT"
 echo "********************************************************"
-while ! `nc -z database $DATABASE_PORT`; do sleep 3; done
+while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
 echo "******** Database Server has started "
 
 echo "********************************************************"
@@ -17,7 +16,6 @@ echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
 while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
 echo "*******  Configuration Server has started"
-
 
 echo "********************************************************"
 echo "Starting Authentication Service                           "
