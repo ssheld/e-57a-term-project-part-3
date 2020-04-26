635b4e7cc3879eb4c1643baf5fcb41721e90fff6
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-26 15:50:58 -0400

    Implemented OAuth2RestTemplate

--- assignment-3.1/assets-service/src/main/java/com/stephensheldon/assets/Application.java
+++ assignment-3.1/assets-service/src/main/java/com/stephensheldon/assets/Application.java
@@ -1,16 +1,15 @@
 package com.stephensheldon.assets;
 
-import com.stephensheldon.assets.utils.UserContextInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Collections;
-import java.util.List;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * Author: Stephen Sheldon
@@ -19,24 +18,14 @@ import java.util.List;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableResourceServer
 public class Application {
 
     @LoadBalanced
     @Bean
-    public RestTemplate getRestTemplate(){
-        RestTemplate template = new RestTemplate();
-        List interceptors = template.getInterceptors();
-
-        // Adding the UserContextInterceptor to the RestTemplate instance that has been created.
-        if (interceptors==null){
-            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
-        }
-        else{
-            interceptors.add(new UserContextInterceptor());
-            template.setInterceptors(interceptors);
-        }
-
-        return template;
+    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext,
+                                                 OAuth2ProtectedResourceDetails details) {
+        return new OAuth2RestTemplate(details, oauth2ClientContext);
     }
 
     public static void main(String[] args) {
