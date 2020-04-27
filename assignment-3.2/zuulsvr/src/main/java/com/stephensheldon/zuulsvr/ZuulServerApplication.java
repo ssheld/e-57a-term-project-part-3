f5cfafe84dc6aa387998d4b33055444434fa7345
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-27 13:54:10 -0400

    Wrote zuul filters

--- assignment-3.2/zuulsvr/src/main/java/com/stephensheldon/zuulsvr/ZuulServerApplication.java
+++ assignment-3.2/zuulsvr/src/main/java/com/stephensheldon/zuulsvr/ZuulServerApplication.java
@@ -1,7 +1,27 @@
 package com.stephensheldon.zuulsvr;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * Author: Stephen Sheldon
  **/
+
+@SpringBootApplication
+@EnableZuulProxy
 public class ZuulServerApplication {
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
 }
