4941c64f3e02196fa39e527217ef23c57f8b97dd
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-27 13:13:07 -0400

    added endpoint config

--- assignment-3.1/authentication-service/src/main/java/com/stephensheldon/authentication/security/OAuth2Config.java
+++ assignment-3.1/authentication-service/src/main/java/com/stephensheldon/authentication/security/OAuth2Config.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 /**
  * Author: Stephen Sheldon
@@ -13,7 +14,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 
 @Configuration
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
-
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -29,5 +29,14 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
                 .secret("thisissecret") // Set secret for our ClientBuilder "eagleeye"
                 .authorizedGrantTypes("refresh_token", "password", "client_credentials") // Set authorized grant types on our ClientBuilder
                 .scopes("webclient", "mobileclient"); // Set scope to web client and mobile client
+    }
+
+    // Configure the non-security features of the Authorization Server endpoints, like token store, token customizations,
+    // user approvals and grant types.
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .authenticationManager(authenticationManager) //use injected authenticationManager to process Auth requests.
+                .userDetailsService(userDetailsService); //set userDetailsService
     }
 }
