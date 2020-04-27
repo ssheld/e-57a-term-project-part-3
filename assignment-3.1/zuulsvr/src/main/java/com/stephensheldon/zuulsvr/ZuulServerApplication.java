8626ea6d906bb9ba966684a71c447febd929dc71
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-27 13:46:53 -0400

    Removed UserContextInterceptor.java and related code

--- assignment-3.1/zuulsvr/src/main/java/com/stephensheldon/zuulsvr/ZuulServerApplication.java
+++ assignment-3.1/zuulsvr/src/main/java/com/stephensheldon/zuulsvr/ZuulServerApplication.java
@@ -1,15 +1,11 @@
 package com.stephensheldon.zuulsvr;
 
-import com.stephensheldon.zuulsvr.utils.UserContextInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Author: Stephen Sheldon
@@ -20,17 +16,8 @@ public class ZuulServerApplication {
 
     @LoadBalanced
     @Bean
-    public RestTemplate getRestTemplate(){
-        RestTemplate template = new RestTemplate();
-        List interceptors = template.getInterceptors();
-        if (interceptors == null) {
-            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
-        } else {
-            interceptors.add(new UserContextInterceptor());
-            template.setInterceptors(interceptors);
-        }
-
-        return template;
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 
     public static void main(String[] args) {
