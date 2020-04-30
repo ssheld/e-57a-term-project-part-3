03f1af1070498850696873bed86d5aec506e3c0e
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-30 12:04:11 -0400

    Created model classes for assets-service

---
+++ assignment-3.2/assets-service/src/main/java/com/stephensheldon/assets/model/Organization.java
@@ -0,0 +1,65 @@
+package com.stephensheldon.assets.model;
+
+/**
+ * Author: Stephen Sheldon
+ **/
+
+public class Organization {
+
+    private String id;
+    private String organizationName;
+    private String contactName;
+    private String contactEmail;
+    private String contactPhone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    @Override
+    public String toString() {
+        return "Organization{" +
+                "id='" + id + '\'' +
+                ", organizationName='" + organizationName + '\'' +
+                ", contactName='" + contactName + '\'' +
+                ", contactEmail='" + contactEmail + '\'' +
+                ", contactPhone='" + contactPhone + '\'' +
+                '}';
+    }
+}
