b7c678e9cf7103cb60d449c1074a3bf133ac735d
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-30 12:29:13 -0400

    Implemented hystrix/correlation-id related classes.

---
+++ assignment-3.2/assets-service/src/main/java/com/stephensheldon/assets/hystrix/DelegatingUserContextCallable.java
@@ -0,0 +1,41 @@
+package com.stephensheldon.assets.hystrix;
+
+import com.stephensheldon.assets.utils.UserContext;
+import com.stephensheldon.assets.utils.UserContextHolder;
+import org.springframework.util.Assert;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Author: Stephen Sheldon
+ **/
+
+public final class DelegatingUserContextCallable<V> implements Callable<V> {
+    private final Callable<V> delegate;
+
+    private UserContext originalUserContext;
+
+    public DelegatingUserContextCallable(Callable<V> delegate,
+                                         UserContext userContext) {
+        Assert.notNull(delegate, "delegate cannot be null");
+        Assert.notNull(userContext, "userContext cannot be null");
+        this.delegate = delegate;
+        this.originalUserContext = userContext;
+    }
+
+    public V call() throws Exception {
+        UserContextHolder.setContext(originalUserContext);
+
+        try {
+            return delegate.call();
+        } finally {
+            this.originalUserContext = null;
+        }
+    }
+
+    public static <V> Callable<V> create(Callable<V> delegate,
+                                         UserContext userContext) {
+        return new DelegatingUserContextCallable<>(delegate, userContext);
+    }
+}
+
---
+++ assignment-3.2/assets-service/src/main/java/com/stephensheldon/assets/utils/UserContext.java
@@ -0,0 +1,53 @@
+package com.stephensheldon.assets.utils;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Author: Stephen Sheldon
+ **/
+
+@Component
+public class UserContext {
+    public static final String CORRELATION_ID = "tmx-correlation-id";
+    public static final String AUTH_TOKEN = "tmx-auth-token";
+    public static final String USER_ID = "tmx-user-id";
+    public static final String ORG_ID = "tmx-org-id";
+
+    private static final ThreadLocal<String> correlationId = new ThreadLocal<>();
+    private static final ThreadLocal<String> authToken = new ThreadLocal<>();
+    private static final ThreadLocal<String> userId = new ThreadLocal<>();
+    private static final ThreadLocal<String> orgId = new ThreadLocal<>();
+
+    public String getCorrelationId() {
+        return correlationId.get();
+    }
+
+    public void setCorrelationId(String cid) {
+        correlationId.set(cid);
+    }
+
+    public String getAuthToken() {
+        return authToken.get();
+    }
+
+    public void setAuthToken(String aToken) {
+        authToken.set(aToken);
+    }
+
+    public String getUserId() {
+        return userId.get();
+    }
+
+    public void setUserId(String aUser) {
+        userId.set(aUser);
+    }
+
+    public String getOrgId() {
+        return orgId.get();
+    }
+
+    public void setOrgId(String aOrg) {
+        orgId.set(aOrg);
+    }
+}
+
