86057923c8b6a4540f848e34a0bc3a2f6f3a375f
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-30 11:23:40 -0400

    Created main service class

---
+++ assignment-3.2/eurekasvr/src/main/java/com/stephensheldon/eurekasvr/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.stephensheldon.eurekasvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Author: Stephen Sheldon
+ **/
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
