186f859457e2dd48fa8a04ece814df6305efb6a3
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-30 11:08:32 -0400

    Created main confsvr class

---
+++ assignment-3.2/confsvr/src/main/java/com/stephensheldon/confsvr/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.stephensheldon.confsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Author: Stephen Sheldon
+ **/
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
