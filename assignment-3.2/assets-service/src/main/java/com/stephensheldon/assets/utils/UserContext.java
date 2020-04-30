5803c951eca76de203fe2efc02889e018fc4fb8c
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-30 14:10:00 -0400

    Updated methods to be static and removed oauth related code

--- assignment-3.2/assets-service/src/main/java/com/stephensheldon/assets/utils/UserContext.java
+++ assignment-3.2/assets-service/src/main/java/com/stephensheldon/assets/utils/UserContext.java
@@ -9,45 +9,34 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserContext {
     public static final String CORRELATION_ID = "tmx-correlation-id";
-    public static final String AUTH_TOKEN = "tmx-auth-token";
     public static final String USER_ID = "tmx-user-id";
     public static final String ORG_ID = "tmx-org-id";
 
     private static final ThreadLocal<String> correlationId = new ThreadLocal<>();
-    private static final ThreadLocal<String> authToken = new ThreadLocal<>();
     private static final ThreadLocal<String> userId = new ThreadLocal<>();
     private static final ThreadLocal<String> orgId = new ThreadLocal<>();
 
-    public String getCorrelationId() {
+    public static String getCorrelationId() {
         return correlationId.get();
     }
 
-    public void setCorrelationId(String cid) {
+    public static void setCorrelationId(String cid) {
         correlationId.set(cid);
     }
 
-    public String getAuthToken() {
-        return authToken.get();
-    }
-
-    public void setAuthToken(String aToken) {
-        authToken.set(aToken);
-    }
-
-    public String getUserId() {
+    public static String getUserId() {
         return userId.get();
     }
 
-    public void setUserId(String aUser) {
+    public static void setUserId(String aUser) {
         userId.set(aUser);
     }
 
-    public String getOrgId() {
+    public static String getOrgId() {
         return orgId.get();
     }
 
-    public void setOrgId(String aOrg) {
+    public static void setOrgId(String aOrg) {
         orgId.set(aOrg);
     }
 }
-
