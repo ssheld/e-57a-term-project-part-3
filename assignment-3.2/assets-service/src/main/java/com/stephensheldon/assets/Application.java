1ac0892de4a43245f1e7129fca0c626300966c6e
Stephen <20730278+ssheld@users.noreply.github.com>
2020-04-30 13:34:52 -0400

    Added rest template code for jedis/redis

--- assignment-3.2/assets-service/src/main/java/com/stephensheldon/assets/Application.java
+++ assignment-3.2/assets-service/src/main/java/com/stephensheldon/assets/Application.java
@@ -1,12 +1,21 @@
 package com.stephensheldon.assets;
 
 import com.stephensheldon.assets.config.ServiceConfig;
+import com.stephensheldon.assets.utils.UserContextInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Author: Stephen Sheldon
@@ -21,5 +30,37 @@ public class Application {
     @Autowired
     private ServiceConfig serviceConfig;
 
+    public RestTemplate getRestTemplate() {
+        RestTemplate template = new RestTemplate();
+        List interceptors = template.getInterceptors();
+        if (interceptors == null) {
+            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        } else {
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+        return template;
+    }
+
+    // Wire JedisConnectionFactory as Spring bean in order to communicate with Redis instance
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConnFactory = new JedisConnectionFactory();
+        jedisConnFactory.setHostName(serviceConfig.getRedisServer());
+        jedisConnFactory.setPort(serviceConfig.getRedisPort());
+        return jedisConnFactory;
+    }
+
+    // Rest template to communicate data to Redis
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
