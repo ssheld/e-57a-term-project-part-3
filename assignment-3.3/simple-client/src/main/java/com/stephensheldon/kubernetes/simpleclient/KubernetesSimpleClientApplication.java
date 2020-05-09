e02d338343d06b1f43b815637504d2694ba93b1f
Stephen <20730278+ssheld@users.noreply.github.com>
2020-05-09 10:44:58 -0400

    Project commit

--- assignment-3.3/simple-client/src/main/java/com/stephensheldon/kubernetes/simpleclient/KubernetesSimpleClientApplication.java
+++ assignment-3.3/simple-client/src/main/java/com/stephensheldon/kubernetes/simpleclient/KubernetesSimpleClientApplication.java
@@ -1,7 +1,38 @@
 package com.stephensheldon.kubernetes.simpleclient;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.UnknownHostException;
+
 /**
  * Author: Stephen Sheldon
  **/
+
+@RestController
+@SpringBootApplication
 public class KubernetesSimpleClientApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(KubernetesSimpleClientApplication.class);
+
+    @GetMapping(value = "/")
+    public String get() throws UnknownHostException {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String resourceUrl = "http://simple-server:8080";
+
+        ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl, String.class);
+        return String.format("Message from simple server is: " + response.getBody());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesSimpleClientApplication.class, args);
+    }
 }
--- assignment-3.3/simple-server/src/main/java/com/stephensheldon/kubernetes/simpleserver/KubernetesSimpleServerApplication.java
+++ assignment-3.3/simple-server/src/main/java/com/stephensheldon/kubernetes/simpleserver/KubernetesSimpleServerApplication.java
@@ -1,7 +1,35 @@
 package com.stephensheldon.kubernetes.simpleserver;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Author: Stephen Sheldon
  **/
+
+@RestController
+@SpringBootApplication
 public class KubernetesSimpleServerApplication {
+
+    @GetMapping(value = "/")
+    public String get() throws UnknownHostException {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("The simple server hostname is ")
+                .append(InetAddress
+                        .getLocalHost()
+                        .getHostName())
+                .append(InetAddress
+                        .getLocalHost()
+                        .getHostAddress());
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesSimpleServerApplication.class, args);
+    }
 }
